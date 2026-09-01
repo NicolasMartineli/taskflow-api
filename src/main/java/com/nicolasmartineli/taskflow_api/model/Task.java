1ff2220da358c047788dcc3f8b4fb137679991cf
Nicolas <nicolaslmartineli@gmail.com>
2026-09-01 11:54:18 -0300

    feat: create Task entity

---
+++ src/main/java/com/nicolasmartineli/taskflow_api/model/Task.java
@@ -0,0 +1,45 @@
+package com.nicolasmartineli.taskflow_api.model;
+
+import com.nicolasmartineli.taskflow_api.model.enums.TaskPriority;
+import com.nicolasmartineli.taskflow_api.model.enums.TaskStatus;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "tasks")
+public class Task extends BaseEntity {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "title", nullable = false, length = 100)
+    private String title;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status")
+    private TaskStatus status;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "priority")
+    private TaskPriority priority;
+
+    @Column(name = "due_date")
+    private LocalDate dueDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "project_id", nullable = false)
+    private Project project;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "assignee_id")
+    private User assignee;
+
+}
