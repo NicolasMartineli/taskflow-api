43785032714869fc8bd6e0e15456e3fc5b58cd06
Nicolas <nicolaslmartineli@gmail.com>
2026-09-01 17:46:29 -0300

    feat: implement user creation in UserService with duplicate email exception

---
+++ src/main/java/com/nicolasmartineli/taskflow_api/exceptions/EmailAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.nicolasmartineli.taskflow_api.exceptions;
+
+public class EmailAlreadyExistsException extends RuntimeException {
+    public EmailAlreadyExistsException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/nicolasmartineli/taskflow_api/services/UserService.java
@@ -0,0 +1,32 @@
+package com.nicolasmartineli.taskflow_api.services;
+
+import com.nicolasmartineli.taskflow_api.dtos.UserCreateRequest;
+import com.nicolasmartineli.taskflow_api.dtos.UserResponse;
+import com.nicolasmartineli.taskflow_api.exceptions.EmailAlreadyExistsException;
+import com.nicolasmartineli.taskflow_api.mappers.UserMapper;
+import com.nicolasmartineli.taskflow_api.models.User;
+import com.nicolasmartineli.taskflow_api.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserMapper mapper;
+    private final UserRepository repository;
+
+    public UserResponse create(UserCreateRequest request) {
+
+        if (repository.existsByEmail(request.email())) {
+            throw new EmailAlreadyExistsException("This email already exists");
+
+        }
+        User entity = mapper.toEntity(request);
+
+        User user = repository.save(entity);
+
+        return mapper.toResponse(user);
+
+    }
+}
