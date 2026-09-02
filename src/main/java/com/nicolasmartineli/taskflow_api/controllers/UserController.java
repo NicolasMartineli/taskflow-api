c878ad2cebb9915a9235fc51fdb278d5a6165aaa
Nicolas <nicolaslmartineli@gmail.com>
2026-09-02 16:26:44 -0300

    feat: implement User creation endpoint (POST)

--- src/main/java/com/nicolasmartineli/taskflow_api/controllers/UserController.java
+++ src/main/java/com/nicolasmartineli/taskflow_api/controllers/UserController.java
@@ -1,4 +1,33 @@
-package com.nicolasmartineli.taskflow_api.controlllers;
+package com.nicolasmartineli.taskflow_api.controllers;
 
-public class UserController {
+import com.nicolasmartineli.taskflow_api.dtos.UserCreateRequest;
+import com.nicolasmartineli.taskflow_api.dtos.UserResponse;
+import com.nicolasmartineli.taskflow_api.services.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("users")
+@RequiredArgsConstructor
+public class UserController implements LocationHeaderUriBuilder {
+
+    private final UserService service;
+
+    @PostMapping
+    public ResponseEntity<Void> create(@RequestBody @Valid UserCreateRequest user) {
+
+        UserResponse userResponse = service.create(user);
+
+        URI location = buildLocationUri(userResponse.id());
+
+        return ResponseEntity.created(location).build();
+
+    }
 }
