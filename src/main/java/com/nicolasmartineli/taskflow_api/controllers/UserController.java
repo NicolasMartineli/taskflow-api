259b32acf987288364951e416d7f4c605a1d5584
Nicolas <nicolaslmartineli@gmail.com>
2026-09-03 17:21:03 -0300

    feat: implement user finds, update and delete endpoints

--- src/main/java/com/nicolasmartineli/taskflow_api/controllers/UserController.java
+++ src/main/java/com/nicolasmartineli/taskflow_api/controllers/UserController.java
@@ -2,16 +2,18 @@ package com.nicolasmartineli.taskflow_api.controllers;
 
 import com.nicolasmartineli.taskflow_api.dtos.UserCreateRequest;
 import com.nicolasmartineli.taskflow_api.dtos.UserResponse;
+import com.nicolasmartineli.taskflow_api.dtos.UserUpdateRequest;
 import com.nicolasmartineli.taskflow_api.services.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("users")
@@ -30,4 +32,40 @@ public class UserController implements LocationHeaderUriBuilder {
         return ResponseEntity.created(location).build();
 
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponse> findById(@PathVariable UUID id) {
+
+        UserResponse user = service.findById(id);
+
+        return ResponseEntity.ok(user);
+
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<UserResponse>> findByNameAndEmail(
+            @RequestParam(value = "name", required = false) String name,
+            @RequestParam(value = "email", required = false) String email,
+            @PageableDefault(page = 0, size = 10, sort = "name") Pageable pageable) {
+
+        Page<UserResponse> byNameAndEmail = service.findByNameAndEmail(name, email, pageable);
+
+        return ResponseEntity.ok(byNameAndEmail);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable UUID id) {
+        service.delete(id);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> update(@PathVariable UUID id, @Valid @RequestBody UserUpdateRequest user) {
+
+        service.update(id, user);
+
+        return ResponseEntity.noContent().build();
+    }
+
 }
