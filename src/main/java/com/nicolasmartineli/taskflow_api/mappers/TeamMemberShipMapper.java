f4d5f1216690b69e4c3b5d81a7419dc1db5b71f9
Nicolas <nicolaslmartineli@gmail.com>
2026-09-06 20:25:13 -0300

    feat: add TeamMembership mapper

--- src/main/java/com/nicolasmartineli/taskflow_api/mappers/TeamMemberShipMapper.java
+++ src/main/java/com/nicolasmartineli/taskflow_api/mappers/TeamMemberShipMapper.java
@@ -1,4 +1,22 @@
 package com.nicolasmartineli.taskflow_api.mappers;
 
+import com.nicolasmartineli.taskflow_api.dtos.TeamMembershipCreateRequest;
+import com.nicolasmartineli.taskflow_api.models.Team;
+import com.nicolasmartineli.taskflow_api.models.TeamMembership;
+import com.nicolasmartineli.taskflow_api.models.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface TeamMemberShipMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "roleInTeam", source = "teamMembershipCreateRequest.roleInTeam")
+    @Mapping(target = "team", source = "team")
+    @Mapping(target = "user", source = "user")
+    TeamMembership toEntity(
+            TeamMembershipCreateRequest teamMembershipCreateRequest,
+            Team team,
+            User user);
 }
