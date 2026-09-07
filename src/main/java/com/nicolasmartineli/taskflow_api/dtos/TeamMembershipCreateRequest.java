dacae857d7afb34b0cf560df4479ab4eca141c33
Nicolas <nicolaslmartineli@gmail.com>
2026-09-07 12:13:23 -0300

    fix: remove invalid @UUID annotation from TeamMembershipCreateRequest

--- src/main/java/com/nicolasmartineli/taskflow_api/dtos/TeamMembershipCreateRequest.java
+++ src/main/java/com/nicolasmartineli/taskflow_api/dtos/TeamMembershipCreateRequest.java
@@ -2,12 +2,14 @@ package com.nicolasmartineli.taskflow_api.dtos;
 
 import com.nicolasmartineli.taskflow_api.models.enums.TeamRole;
 import jakarta.validation.constraints.NotNull;
-import org.hibernate.validator.constraints.UUID;
+
+import java.util.UUID;
+
 
 public record TeamMembershipCreateRequest(
-        @UUID(message = "Invalid format for id")
+
         @NotNull(message = "User id is required")
-        java.util.UUID userId,
+        UUID userId,
 
         @NotNull(message = "Role in team is required")
         TeamRole roleInTeam
